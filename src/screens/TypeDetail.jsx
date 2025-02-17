044a1d2ebb4fd45d9182cfa41a802db2d4c2e8e4
Apfirebolt <aspper20@gmail.com>
2025-02-17 10:14:01 +0530

    feat: type detail page

--- src/screens/TypeDetail.jsx
+++ src/screens/TypeDetail.jsx
@@ -1,108 +1,79 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { getSinglePokemon } from "../features/pokemon/pokemonSlice";
+import { getSingleType } from "../features/type/typeSlice";
 import Loader from "../components/Loader";
 
 const TypeDetail = () => {
-    const { id } = useParams();
-    const { type, isLoading } = useSelector((state) => state.pokemonData);
+  const { id } = useParams();
+  const { type, isLoading } = useSelector((state) => state.typeData);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const capitalize = (str) => {
-        if (str) {
-            return str.charAt(0).toUpperCase() + str.slice(1);
-        }
-        return "";
-    };
-
-    useEffect(() => {
-        dispatch(getSinglePokemon(id));
-    }, [dispatch, id]);
-
-    if (isLoading) {
-        return <Loader />;
+  const capitalize = (str) => {
+    if (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
     }
+    return "";
+  };
 
-    return (
-        type && (
-            <div className="max-w-xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden my-5">
-                <div className="p-4">
-                    <div className="flex justify-between items-center mb-4">
-                        <h1 className="text-2xl font-bold mb-2">{capitalize(type.name)}</h1>
-                        <p className="text-gray-700 mb-2">Height: {type.height}</p>
-                        <p className="text-gray-700 mb-2">Weight: {type.weight}</p>
-                    </div>
+  useEffect(() => {
+    dispatch(getSingleType(id));
+  }, [dispatch, id]);
 
-                    {type.sprites && (
-                        <div className="mb-4">
-                            <h2 className="text-xl font-semibold mb-2">Sprites</h2>
-                            <div className="flex flex-wrap">
-                                {Object.keys(type.sprites).map((key) => {
-                                    if (type.sprites[key] && typeof type.sprites[key] === "string") {
-                                        return (
-                                            <div key={key} className="m-2">
-                                                <img src={type.sprites[key]} alt={key} className="w-20 h-20" />
-                                                <p className="text-center text-gray-600">{capitalize(key.replace(/_/g, " "))}</p>
-                                            </div>
-                                        );
-                                    }
-                                    return null;
-                                })}
-                            </div>
-                        </div>
-                    )}
+  if (isLoading) {
+    return <Loader />;
+  }
 
-                    {type.stats && type.stats.length > 0 && (
-                        <div className="mb-4">
-                            <h2 className="text-xl font-semibold mb-2">Stats</h2>
-                            <ul className="list-disc list-inside">
-                                {type.stats.map((stat) => (
-                                    <li key={stat.stat.name}>
-                                        {capitalize(stat.stat.name)}: {stat.base_stat}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    )}
+  return (
+    type && (
+      <div className="max-w-xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden my-5">
+        <div className="p-4">
+          <div className="flex justify-between items-center mb-4">
+            <h1 className="text-2xl font-bold mb-2">{capitalize(type.name)}</h1>
+          </div>
 
-                    {type.types && type.types.length > 0 && (
-                        <div className="mb-4">
-                            <h2 className="text-xl font-semibold mb-2">Types</h2>
-                            <ul className="list-disc list-inside">
-                                {type.types.map((type) => (
-                                    <li key={type.type.name}>{capitalize(type.type.name)}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    )}
-
-                    {type.abilities && type.abilities.length > 0 && (
-                        <div className="mb-4">
-                            <h2 className="text-xl font-semibold mb-2">Abilities</h2>
-                            <ul className="list-disc list-inside">
-                                {type.abilities.map((ability) => (
-                                    <li key={ability.ability.name}>{capitalize(ability.ability.name)}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    )}
-
-                    {type.moves && type.moves.length > 0 && (
-                        <div className="mb-4">
-                            <h2 className="text-xl font-semibold mb-2">Moves</h2>
-                            <ul className="list-none">
-                                {type.moves.slice(0, 10).map((move) => (
-                                    <li className="my-1 px-2 py-3 bg-neutral-200 text-gray-600" key={move.move.name}>{capitalize(move.move.name)}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    )}
-                </div>
+          {type.damage_relations && (
+            <div className="mb-4">
+              <h2 className="text-xl font-semibold mb-2">Damage Relations</h2>
+              <div className="flex flex-wrap">
+                {Object.keys(type.damage_relations).map((key) => (
+                  <div key={key} className="m-2">
+                    <h3 className="text-lg font-medium">
+                      {capitalize(key.replace(/_/g, " "))}
+                    </h3>
+                    <ul className="list-disc list-inside">
+                      {type.damage_relations[key].map((relation) => (
+                        <li key={relation.name}>{capitalize(relation.name)}</li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
             </div>
-        )
-    );
+          )}
+
+          {type.pokemon && type.pokemon.length > 0 && (
+            <div className="mb-4">
+              <h2 className="text-xl font-semibold mb-2">Pokemon</h2>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {type.pokemon.map((poke) => (
+                  <div
+                    key={poke.pokemon.name}
+                    className="bg-gray-100 p-4 rounded-lg shadow-md"
+                  >
+                    <h3 className="text-lg font-medium">
+                      {capitalize(poke.pokemon.name)}
+                    </h3>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    )
+  );
 };
 
 export default TypeDetail;
